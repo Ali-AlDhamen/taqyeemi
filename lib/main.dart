523505b690fa6da343b59fd8ce52fd83acaef829
Pali2002 <ali77dhamen@hotmail.com>
2023-03-03 17:08:01 +0300

    switching pages depends on login status done

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-
 import 'package:firebase_core/firebase_core.dart';
+import 'package:taqyeemi/core/common/loader.dart';
+import 'package:taqyeemi/screens/instructors_screen.dart';
+import 'core/common/error_text.dart';
+import 'features/auth/controller/auth_controller.dart';
 import 'features/auth/screens/login_screen.dart';
 import 'features/auth/screens/signup_screen.dart';
 import 'firebase_options.dart';
@@ -13,22 +16,33 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const ProviderScope(child:  MyApp()));
+  runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: Pallete.darkModeAppTheme,
       title: 'Taqyeemi',
-      home: const SignInScreen(),
+      home: ref.watch(authStateChangeProvider).when(
+            data: (data) {
+              if (data != null) {
+                return const InstructorsScreen();
+              } else {
+                return const SignInScreen();
+              }
+            },
+            error: (error, StackTrace) => ErrorText(error: error.toString()),
+            loading: () => const Loader(),
+          ),
       routes: {
         SignUpScreen.routeName: (context) => const SignUpScreen(),
         SignInScreen.routeName: (context) => const SignInScreen(),
+        InstructorsScreen.routeName: (context) => const InstructorsScreen()
       },
     );
   }
