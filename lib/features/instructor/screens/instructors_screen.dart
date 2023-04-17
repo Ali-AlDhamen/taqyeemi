a1e22165984c815bdf74e305da9ffefa557cc742
Ali-AlDhamen <ali77dhamen@hotmail.com>
2023-04-18 01:23:05 +0300

    instructors screen

--- lib/features/instructor/screens/instructors_screen.dart
+++ lib/features/instructor/screens/instructors_screen.dart
@@ -1,11 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:taqyeemi/features/instructor/controller/instructor_controller.dart';
+import 'package:taqyeemi/features/instructor/screens/widgets/instructor_information.dart';
+
+import '../../../core/core.dart';
 
 class InstructorsScreen extends ConsumerWidget {
   const InstructorsScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+    return Center(
+      child: Column(
+        children: [
+          ref.watch(instructorsProvider).when(
+                data: (instructors) {
+                  
+                  print(instructors);
+                  return Expanded(
+                    child: ListView.builder(
+                      itemCount: instructors.length,
+                      itemBuilder: (context, index) {
+                        return InstructorInformation(
+                          instructor: instructors[index],
+                        );
+                      },
+                    ),
+                  );
+                },
+                error: (error, stackTrace) => ErrorText(error: stackTrace.toString()),
+                loading: () => const Loader(),
+              ),
+        ],
+      ),
+    );
   }
 }
