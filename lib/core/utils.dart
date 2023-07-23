d55045d9f73514a01b799235cc1467f5b930c98f
Ali-AlDhamen <ali77dhamen@hotmail.com>
2023-07-23 03:34:06 +0300

    emailValidator

--- lib/core/utils.dart
+++ lib/core/utils.dart
@@ -400,3 +400,14 @@ double getCourseDiffucltyInNumber(List<CourseComment> comments){
   }
   return sum / comments.length;
 }
+
+
+String? emailValidator(String? value) {
+  if (value == null || value.isEmpty) {
+    return 'Please enter your email';
+  }
+  if (!value.contains('@') || !value.contains('.')) {
+    return 'Please enter a valid email';
+  }
+  return null;
+}
